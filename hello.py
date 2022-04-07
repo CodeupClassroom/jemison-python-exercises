064635d0698911e708c06b1ce8827e98a99414b9
Ryan Orsinger <ryan.orsinger@gmail.com>
2022-04-07 17:35:59 -0500

    intro material

---
+++ hello.py
@@ -0,0 +1,9 @@
+# Comments are lines of text that are not executed by the programming language
+# print("hello!")
+# Comments are for human beings
+
+# Prompt the user for their name
+# input is a function that prints what's in the parentheses and returns the input from the user
+name = input("Hello, what is your name?")
+
+print("Hello", name)
